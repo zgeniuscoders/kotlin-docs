f2006d824e62845b99aff4c54487bcf52b931107
zgeniuscoders <zgeniuscoders.com>
2024-04-21 14:38:01 +0100

    null safety'

--- app.config.ts
+++ app.config.ts
@@ -1,24 +1,13 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
+import { Title } from './.nuxt/components.d';
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'KOTLIN DOCS',
+    description: 'Mini documentation kotlin',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      twitter: 'zgeniuscoders',
+      github: 'https://github.com/zgeniuscoders/',
     },
     aside: {
       level: 0,
